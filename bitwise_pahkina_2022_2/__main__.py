83c5e7f2d97fb6c1387c9e575c90a37b311924e0
Mikko Kouhia <mikko.kouhia@iki.fi>
2022-05-01 14:31:47 +0300

    Add command line, callbacks, logging

--- bitwise_pahkina_2022_2/__main__.py
+++ bitwise_pahkina_2022_2/__main__.py
@@ -1,31 +1,68 @@
 """Command line interface"""
 
+import argparse
 from pathlib import Path
 
 from .network import BaseNetwork
 from .optimize import optimize
 
 
-def main():
-    """Generate base network, create variations, find best solution"""
-    base_net = BaseNetwork.from_json(Path(__file__).parent / "koodipahkina-data.json")
-    res = optimize(base_net, termination={"n_max_gen": 20}, seed=1, verbose=True)
+def main(argv: list[str] = None):
+    """Generate base network, create variations, find best solution
 
-    print(f"Best solution found: \nF = {res.F}")
+    Args:
+        argv (list[str]): List of command line arguments. Defaults to None.
+    """
+    parsed_args = _parse_args(argv)
+
+    base_net = BaseNetwork.from_json(Path(__file__).parent / "koodipahkina-data.json")
+    res = optimize(
+        base_net,
+        termination={"n_max_gen": parsed_args.max_gen},
+        seed=1,
+        verbose=not parsed_args.quiet,
+        x_path=parsed_args.xpath,
+        metric_log=parsed_args.metric_log,
+        resume=parsed_args.resume,
+    )
+
+    if parsed_args.quiet:
+        return
 
     best_x_binary = res.opt.get("pheno")[0]
     removed_edges = (best_x_binary == 0).nonzero()[0]
     new_net = base_net.as_graph(remove_edges=removed_edges.tolist())
-
     score = base_net.comparison_score(new_net)
-    print(f"Best score: {score:.3f}")
 
-    print(
-        "Solution:",
-    )
+    print(f"\nBest score: {score:.3f}")
+    print(f"Execution time: {res.exec_time:.2f} s")
+    print("Solution:")
     print(", ".join(removed_edges.astype(str)))
 
-    print(res.exec_time)
+
+def _parse_args(args: list[str]) -> argparse.Namespace:
+    """Parse arguments from command line"""
+    parser = argparse.ArgumentParser(description="""Optimize liana network.""")
+    parser.add_argument(
+        "--max-gen",
+        default=75,
+        type=int,
+        help="Maximum number of generations for genetic algorithm. Default: 75",
+    )
+    parser.add_argument(
+        "--metric-log",
+        default="opt_log.txt",
+        help="Location for logging optimization running metrics. Default: opt_log.txt",
+    )
+    parser.add_argument(
+        "--xpath",
+        default="opt_X_latest.npy",
+        help="Location for saving intermediate x values for population. Default: opt_X_latest.npy",
+    )
+    parser.add_argument("--resume", action="store_true", help="Resume from xfile")
+    parser.add_argument("--quiet", "-q", action="store_true", help="Suppress output")
+
+    return parser.parse_args(args)
 
 
 if __name__ == "__main__":
--- bitwise_pahkina_2022_2/optimize.py
+++ bitwise_pahkina_2022_2/optimize.py
@@ -1,11 +1,16 @@
 """Optimization implementation"""
 
+import os
+from pathlib import Path
+import time
 import numpy as np
 
 from pymoo.algorithms.soo.nonconvex.brkga import BRKGA
+from pymoo.core.callback import Callback
 from pymoo.core.duplicate import ElementwiseDuplicateElimination
 from pymoo.core.problem import ElementwiseProblem
 from pymoo.core.result import Result
+from pymoo.operators.sampling.rnd import FloatRandomSampling
 from pymoo.optimize import minimize
 from pymoo.util.termination.default import SingleObjectiveDefaultTermination
 
@@ -47,8 +52,55 @@ class MyElementwiseDuplicateElimination(ElementwiseDuplicateElimination):
         return a.get("hash")[0] == b.get("hash")[0]
 
 
+class MyCallback(Callback):
+
+    """Callback on each generation"""
+
+    def __init__(
+        self,
+        x_path: os.PathLike | None = None,
+        metric_log: os.PathLike | None = None,
+    ) -> None:
+        super().__init__()
+
+        self.x_path = Path(x_path)
+        self.metric_log = Path(metric_log)
+
+        for key in ["n_gen", "time", "fopt"]:
+            self.data[key] = []
+        self._init_log()
+
+    def _init_log(self):
+        if self.metric_log.exists():
+            self.metric_log.unlink()
+        else:
+            self.metric_log.parent.mkdir(parents=True, exist_ok=True)
+
+        with open(self.metric_log, "w", encoding="utf8") as metric_file:
+            metric_file.write(",".join(self.data.keys()) + "\n")
+
+    def notify(self, algorithm, **kwargs):
+        self.data["n_gen"].append(algorithm.n_gen)
+        self.data["time"].append(time.time())
+        self.data["fopt"].append(algorithm.pop.get("F").min())
+
+        if self.x_path is not None:
+            population_x = algorithm.pop.get("X")
+            np.save(self.x_path, population_x)
+
+        if self.metric_log is not None:
+            with open(self.metric_log, "a", encoding="utf8") as metric_file:
+                line_vals = (str(self.data[key][-1]) for key in self.data)
+                metric_file.write(",".join(line_vals) + "\n")
+
+
 def optimize(
-    base_network: BaseNetwork, termination: dict | None = None, **kwargs
+    base_network: BaseNetwork,
+    termination: dict | None = None,
+    x_path: os.PathLike | None = None,
+    metric_log: os.PathLike | None = None,
+    resume: bool = False,
+    **kwargs
 ) -> Result:
     """Main optimization method
 
@@ -58,12 +110,20 @@ def optimize(
           for termination criteria. See
           :func:`~pymoo.util.termination.default.SingleObjectiveDefaultTermination`.
           Defaults to None.
+        x_path (os.PathLike | None): location for saving intermediate
+          x values for population.
+        metric_log (os.PathLike | None): location for logging
+          optimization running metrics.
+        resume (bool): continue from previous result in x_path.
+          Defaults to False.
         kwargs: keyword arguments to minimization problem
 
     Returns:
         Result: pymoo optimization result
     """
     problem = MyProblem(base_network)
+
+    sampling = np.load(x_path) if resume else FloatRandomSampling()
 
     population_size = 2 * problem.n_var
 
@@ -73,6 +133,8 @@ def optimize(
         n_mutants=int(population_size * 0.1),
         bias=0.7,
         eliminate_duplicates=MyElementwiseDuplicateElimination(),
+        sampling=sampling,
+        callback=MyCallback(x_path=x_path, metric_log=metric_log),
     )
 
     termination = termination = SingleObjectiveDefaultTermination(**termination)
