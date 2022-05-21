8ed33733712680f8e12320ae254b881cb3162435
Mikko Kouhia <mikko.kouhia@iki.fi>
2022-05-21 06:47:23 +0300

    Bump version to 1.4.1

--- bitwise_challenge_2022_2/__init__.py
+++ bitwise_challenge_2022_2/__init__.py
@@ -1,3 +1,3 @@
 """Jungle liana graph optimization coding challenge"""
 
-__version__ = "0.1.0"
+__version__ = "1.4.1"
--- tests/test_bitwise_pahkina_2022_2.py
+++ tests/test_bitwise_pahkina_2022_2.py
@@ -5,4 +5,4 @@ from bitwise_challenge_2022_2 import __version__
 
 def test_version():
     """Version is unchanged"""
-    assert __version__ == "0.1.0"
+    assert __version__ == "1.4.1"
